8db9f4e6b245944664a67a94fd50ab6cfefd55ba
kirank860 <kkiru889@gmail.com>
2023-07-13 20:08:38 +0530

    about,events

---
+++ src/app/components/Events/Event.jsx
@@ -0,0 +1,124 @@
+import React from 'react'
+import "./Event.css"
+const Event = () => {
+  return (
+    <div>
+      <div className="events-section">
+        <div className="event-title">
+            <h1>Events</h1>
+        </div>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-5">
+                    <div className="event-img">
+                        <img src="image 13.png" alt="" />
+                    </div>
+                    <div className='event-btn'>
+                        <button>UP Coming Event</button> <span>32 Minutes AGO</span>
+                    </div>
+                    <div className="evnet-description">
+                        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                        <p className='p2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos voluptate, dicta praesentium rem  consectetur adipisicing elit. Aliquid dolores labore sint sunt veritatis quisquam reiciendis? Alias hic saepe provident dolor, excepturi vel!</p>
+                    </div>
+                    <div className="event-btn-2">
+                        <button>Learn More</button>
+                    </div>
+                </div>
+                <div className="col-lg-4">
+                  <div className="event-right">
+                  <div className="evnet-right-description">
+                        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                        <p>30 june 2023 ----------</p>
+                        <p className='p2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos voluptate, dicta praesentium rem  consectetur adipisicing elit. Aliquid dolores labore sint sunt veritatis quisquam reiciendis? Alias hic saepe provident dolor, excepturi vel!</p>
+                    </div>
+                    <div className='event-r-btn'>
+                        <button > Learn More</button>
+                    </div>
+                  </div>
+                  <div className="event-right">
+                  <div className="evnet-right-description">
+                        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                        <p>30 june 2023 ----------</p>
+                        <p className='p2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos voluptate, dicta praesentium rem  consectetur adipisicing elit. Aliquid dolores labore sint sunt veritatis quisquam reiciendis? Alias hic saepe provident dolor, excepturi vel!</p>
+                    </div>
+                    <div className='event-r-btn'>
+                        <button >Learn More</button>
+                    </div>
+                  </div>
+                  <div className="event-right">
+                  <div className="evnet-right-description">
+                        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                        <p>30 june 2023 ----------</p>
+                        <p className='p2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos voluptate, dicta praesentium rem  consectetur adipisicing elit. Aliquid dolores labore sint sunt veritatis quisquam reiciendis? Alias hic saepe provident dolor, excepturi vel!</p>
+                    </div>
+                    <div className='event-r-btn'>
+                        <button >Learn More</button>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-3">
+                  hshshsh
+                </div>
+            </div>
+            </div>
+            {/* <div className="row-2">
+              <div className="event-image-2">
+                <img src="Mask group.png" alt="" />
+              </div>
+            
+              <div className="container">
+              <div className="library">
+                <h1>Central Library</h1>
+              </div>
+              <div className="library-para">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa harum, ut ex iure aspernatur id
+                   suscipit numquam quos nostrum corporis laudantium illo aut possimus officia similique porro dolorem
+                    itaque necessitatibus? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
+                     consequuntur quam, magni dolore incidunt unde, excepturi obcaecati sed eveniet sequi debitis 
+                     ipsum vitae nihil placeat ab itaque,
+                   voluptatem corporis minus.</p>
+              </div>
+              <div className="library-btn">
+                <button>Learn More</button>
+              </div>
+              </div>
+            </div> */}
+            {/* <div className="row-3">
+              <div className="director">
+                <div className="container">
+                  <div className="director-heading">
+                    <h1>Message from the Director</h1>
+                  </div>
+                </div>
+                <div className="row-4">
+                 <div className="container">
+                 <div className="col-lg-5">
+                  <div className="person">
+                    <img src="person.png" alt="" />
+                  </div>
+                </div>
+                <div className="col-lg-5 two-para">
+                  <div className="person-para">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, consectetur. Vitae,
+                       consequuntur aliquam! Veritatis tenetur repellendus, iure laudantium quos iusto  Lorem ipsum 
+                       dolor sit amet, consectetur adipisicing elit. Consequuntur maxime sint voluptates explicabo 
+                       laboriosam vero pariatur nostrum porro fugit harum quae, obcaecati odio accusamus nesciunt ratione. 
+                       Ullam vero eaque nesciunt. quo doloribus 
+                       tempora repudiandae beatae quisquam,
+                       molestias earum maxime odio.</p>
+                  </div>
+                  <div className="person-details">
+                    <h2>Dr Abduslam Ahmed</h2>
+                    <p>Rector, Al Jamia Al Islamia</p>
+                  </div>
+                </div>
+                 </div>
+                </div>
+              </div>
+            </div> */}
+      </div>
+    </div>
+  )
+}
+
+export default Event
