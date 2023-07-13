cf9596b1c173144033d92c092cc591f0a9c25270
Abhijith ps <abhijithabi22102001@gmail.com>
2023-07-13 19:28:36 +0530

    footer completed

--- src/app/landingpage/LandingPage.js
+++ src/app/landingpage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import CarouselComponent from "../components/carousel/Carousel";
+import Footer from "../components/footer/Footer";
 
 const LandingPage = () => {
   return (
     <div>
       <CarouselComponent />
+      <Footer />
     </div>
   );
 };
