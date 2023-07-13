c92e74b19517853b243d04316b1b4fff1614843d
Abhijith ps <abhijithabi22102001@gmail.com>
2023-07-13 14:45:06 +0530

    completer header

---
+++ src/app/landingpage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CarouselComponent from "../components/carousel/Carousel";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <CarouselComponent />
+    </div>
+  );
+};
+
+export default LandingPage;
