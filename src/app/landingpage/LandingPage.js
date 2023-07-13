1d9cb6ecab77b60035ce423f5b3212da2e82e855
kirank860 <kkiru889@gmail.com>
2023-07-13 20:08:38 +0530

    about,events

--- src/app/landingpage/LandingPage.js
+++ src/app/landingpage/LandingPage.js
@@ -1,10 +1,14 @@
 import React from "react";
+import About from "../components/about/About";
 import CarouselComponent from "../components/carousel/Carousel";
+import Event from "../components/Events/Event";
 
 const LandingPage = () => {
   return (
     <div>
       <CarouselComponent />
+      <About/>
+      <Event/>
     </div>
   );
 };
