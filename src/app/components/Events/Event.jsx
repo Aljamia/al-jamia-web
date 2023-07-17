884b06ceec17f206769d61f0f14d18c3f49f9237
Abhijith ps <abhijithabi22102001@gmail.com>
2023-07-17 15:28:55 +0530

    completed carousel with video

--- src/app/components/Events/Event.jsx
+++ src/app/components/Events/Event.jsx
@@ -9,45 +9,67 @@ const Event = () => {
         <div className="event-title">
           <h1>Events</h1>
         </div>
-      <div className="event-first-section">
-      <Container>
-        <Row>
-          <Col xl={5}>
-           <div className="event-img">
-            <img src="image 13.png" alt="" />
-           </div>
-           <div className="event-btn">
-            <button>Up Coming Events</button>
-            <span>32 MINUTES AGO</span>
-           </div>
-           <div className="event-des">
-            <h4>Lorem ipsum dolor sit amet corrupti natus perferendis commodi sit?</h4>
-            <p>Lorem, ipsum dolor sit amet nobis molestias!adipisicing elit.corrupti quam libero atque nisi mollitia doloribus distinctio? Sapiente non illo, porro ad eum dicta ducimus aliquam nemo.</p>
-           </div>
-           <div className="learn-btn">
-            <button>Learn More</button>
-           </div>
-          </Col>
-          <Col xl={7}>
-          <h1>ha  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium fuga, vero cumque nulla, quas sapiente assumenda repellat iusto quis similique voluptatibus incidunt amet doloremque nostrum minima esse quae consequuntur aperiam.</h1>
-          </Col>
-        </Row>
-      </Container>
-      </div>
-      <div className="event-section-2">
-        <div className="event-s-img">
-          <img src="Mask group.png" alt="" />
-        </div>
+        <div className="event-first-section">
           <Container>
-          <div className="library">
-          <h2>Central Library</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi eveniet, sit similique eum ipsum fugit tenetur? Aliquid voluptatum perspiciatis eveniet ipsam eius, vero  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, eaque repellat at earum blanditiis soluta est dolor molestiae deserunt! Autem corporis eius velit voluptatem ipsa iusto, reiciendis cum adipisci eos. consequuntur, aliquam maxime accusantium sequi libero error?</p>
-        </div>
-        <div className="learn-btn">
-          <button>Learn More</button>
-        </div>
+            <Row>
+              <Col xl={5}>
+                <div className="event-img">
+                  <img src="image 13.png" alt="" />
+                </div>
+                <div className="event-btn">
+                  <button>Up Coming Events</button>
+                  <span>32 MINUTES AGO</span>
+                </div>
+                <div className="event-des">
+                  <h4>
+                    Lorem ipsum dolor sit amet corrupti natus perferendis
+                    commodi sit?
+                  </h4>
+                  <p>
+                    Lorem, ipsum dolor sit amet nobis molestias!adipisicing
+                    elit.corrupti quam libero atque nisi mollitia doloribus
+                    distinctio? Sapiente non illo, porro ad eum dicta ducimus
+                    aliquam nemo.
+                  </p>
+                </div>
+                <div className="learn-btn">
+                  <button>Learn More</button>
+                </div>
+              </Col>
+              <Col xl={7}>
+                <h1>
+                  ha Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Praesentium fuga, vero cumque nulla, quas sapiente assumenda
+                  repellat iusto quis similique voluptatibus incidunt amet
+                  doloremque nostrum minima esse quae consequuntur aperiam.
+                </h1>
+              </Col>
+            </Row>
           </Container>
-      </div>
+        </div>
+        <div className="event-section-2">
+          <div className="event-s-img">
+            <img src="Mask group.png" alt="" />
+          </div>
+          <Container>
+            <div className="library">
+              <h2>Central Library</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+                eveniet, sit similique eum ipsum fugit tenetur? Aliquid
+                voluptatum perspiciatis eveniet ipsam eius, vero Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Maiores, eaque
+                repellat at earum blanditiis soluta est dolor molestiae
+                deserunt! Autem corporis eius velit voluptatem ipsa iusto,
+                reiciendis cum adipisci eos. consequuntur, aliquam maxime
+                accusantium sequi libero error?
+              </p>
+            </div>
+            <div className="learn-btn">
+              <button>Learn More</button>
+            </div>
+          </Container>
+        </div>
       </div>
     </div>
   );
--- src/app/components/carousel/Carousel.js
+++ src/app/components/carousel/Carousel.js
@@ -1,74 +1,58 @@
-import Carousel from "react-bootstrap/Carousel";
 import "./Carousel.css";
 import React from "react";
-import Image from "next/image";
 import Header from "../header/Header";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const CarouselComponent = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 3000,
+    cssEase: "ease",
+    arrows: false,
+  };
   return (
-    <div>
-      <div className="header">
-        <Header /> {/*imported navbar from header */}
-        <div className="carousel-header">
-          <Carousel indicators controls={false} className="carousel">
-            <Carousel.Item interval={1000}>
-              <div className="d-block w-100">
-                <Image
-                  src="/image 2.png"
-                  alt="First slide"
-                  width={1000}
-                  height={500}
-                  layout="responsive"
-                />
+    <div className="header">
+      <Header /> {/*imported navbar from header */}
+      <div className="header-contents">
+        <div className="header-video">
+          <video
+            autoPlay
+            muted
+            loop
+            playsInline
+            className="fullscreen-video"
+            style={{ width: "100%" }}
+          >
+            <source
+              src="/ENCORE 2022 -GRADUATION CEREMONY- AFTER MOVIE-JWALA 2015 MBBS -GOVERNMENT MEDICAL COLLEGE TRIVANDRUM.mp4"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+          <div className="carousel-overlay">
+            <Slider {...settings}>
+              <div className="carousel-para animate__bounceIn">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
               </div>
-              <Carousel.Caption>
-                <div className="carousel-caption ">
-                  <p className="animate__animated animate__bounceIn">
-                    group of individuals involved in persistent social
-                    interaction, or a large social group sharing the same
-                  </p>
-                </div>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={500}>
-              <div className="d-block w-100">
-                <Image
-                  src="/image 2.png"
-                  alt="Second slide"
-                  width={1000}
-                  height={500}
-                  layout="responsive"
-                />
+              <div className="carousel-para animate__bounceIn">
+                <p className="">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
               </div>
-              <Carousel.Caption>
-                <div className="carousel-caption">
-                  <p className="animate__animated animate__bounceIn">
-                    group of individuals involved in persistent social
-                    interaction, or a large social group sharing the same
-                  </p>
-                </div>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <div className="d-block w-100">
-                <Image
-                  src="/image 2.png"
-                  alt="Third slide"
-                  width={1000}
-                  height={500}
-                  layout="responsive"
-                />
+              <div className="carousel-para animate__bounceIn">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing Lorem ipsum
+                </p>
               </div>
-              <Carousel.Caption>
-                <div className="carousel-caption">
-                  <p className="animate__animated animate__bounceIn">
-                    group of individuals involved in persistent social
-                    interaction, or a large social group sharing the same
-                  </p>
-                </div>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
+            </Slider>
+          </div>
         </div>
       </div>
     </div>
