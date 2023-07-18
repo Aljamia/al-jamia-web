638e2f863bc5a9243fcccb442b05e3bae9b893a5
Abhijith ps <abhijithabi22102001@gmail.com>
2023-07-18 20:36:53 +0530

    Overflow resolved

--- src/app/components/Events/Event.jsx
+++ src/app/components/Events/Event.jsx
@@ -216,29 +216,6 @@ const Event = () => {
             </div>
           </Container>
         </div>
-        <div className="event-section-2">
-          <div className="event-s-img">
-            <img src="Mask group.png" alt="" />
-          </div>
-          <Container>
-            <div className="library">
-              <h2>Central Library</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-                eveniet, sit similique eum ipsum fugit tenetur? Aliquid
-                voluptatum perspiciatis eveniet ipsam eius, vero Lorem ipsum
-                dolor sit amet consectetur adipisicing elit. Maiores, eaque
-                repellat at earum blanditiis soluta est dolor molestiae
-                deserunt! Autem corporis eius velit voluptatem ipsa iusto,
-                reiciendis cum adipisci eos. consequuntur, aliquam maxime
-                accusantium sequi libero error?
-              </p>
-            </div>
-            <div className="learn-btn">
-              <button>Learn More</button>
-            </div>
-          </Container>
-        </div>
       </div>
     </div>
   );
