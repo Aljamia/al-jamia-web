04cd7895349dbb95d7f59e077e87511adc9c7b51
Abhijith ps <abhijithabi22102001@gmail.com>
2023-07-27 13:06:14 +0530

    added file merged main

--- src/app/event/page.js
+++ src/app/event/page.js
@@ -3,8 +3,6 @@ import React from "react";
 import Event from "../components/Events/Event";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
-import PageHeader from "../components/pagesheader/PageHeader";
-import News from "../pages/Event/News";
 import "../components/Events/Event.css";
 
 const page = () => {
