02a940dc9efd3a95e5843b1b8794baab265f8375
Abhijith ps <abhijithabi22102001@gmail.com>
2023-08-07 18:06:36 +0530

    added file facilities

---
+++ src/app/facilities/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Facilities from "../pages/ourfacilities/Facilities";
+
+const page = () => {
+  return (
+    <div>
+      <Facilities />
+    </div>
+  );
+};
+
+export default page;
