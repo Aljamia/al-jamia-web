b72a852b6ff42949112b92ad8dcaae3aeb7f8008
Abhijith ps <abhijithabi22102001@gmail.com>
2023-10-21 18:15:41 +0530

    updates dwnld button

--- src/app/components/Events/Event.jsx
+++ src/app/components/Events/Event.jsx
@@ -119,7 +119,7 @@ const Event = () => {
                         alt={event.name}
                         style={{
                           objectFit: "contain",
-                          width: "10%",
+                          width: "100%",
                           height: "100%",
                         }}
                       />
